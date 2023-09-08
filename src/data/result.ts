9336f7e448d74fad54fa0ccef5e380176ca24bcc
Aloun Vangkeosay <alounv@gmail.com>
2023-09-08 18:07:09 +0200

    Start analytics

--- src/data/result.ts
+++ src/data/result.ts
@@ -2,6 +2,7 @@ import type { NewResult } from "~/server/db/schema";
 import { resultsTable } from "~/server/db/schema";
 import type { NeonDatabase } from "drizzle-orm/neon-serverless";
 import { and, eq, isNull } from "drizzle-orm/expressions";
+import { sql } from "drizzle-orm";
 
 export async function createResult(
   db: NeonDatabase,
@@ -40,4 +41,31 @@ export async function addErrorDate(
     .returning();
 
   return updated;
+}
+
+export async function getAnalyticsPerWord(
+  db: NeonDatabase,
+  { userId, repetitions }: { userId: string; repetitions: number },
+) {
+  const results = await db
+    .select({
+      count: sql<number>`count(${resultsTable.id})`,
+      duration: sql<number>`min(${resultsTable.duration})`,
+      word: resultsTable.word,
+    })
+    .from(resultsTable)
+    .where(and(eq(resultsTable.userId, userId), isNull(resultsTable.errorDate)))
+    .groupBy((r) => r.word);
+
+  const filteredResults = results
+    .filter((r) => r.count >= repetitions)
+    .map((r) => ({
+      word: r.word,
+      speed: ((r.word.length / (r.duration / 1000)) * 60) / 5,
+    }))
+    .sort((a, b) => b.speed - a.speed);
+
+  console.log(filteredResults);
+
+  return filteredResults;
 }
