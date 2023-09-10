a42df59436f5c91323420fe42799696f9566daee
Alounv <34238160+Alounv@users.noreply.github.com>
2023-09-10 22:38:33 +0200

    Take only repetition last results (#11)

--- src/data/result.ts
+++ src/data/result.ts
@@ -1,45 +1,18 @@
 import type { NewResult } from "~/server/db/schema";
 import { resultsTable } from "~/server/db/schema";
-import { and, eq, isNull, sql } from "drizzle-orm";
+import { and, desc, eq, inArray, sql } from "drizzle-orm";
 import type { NeonHttpDatabase } from "drizzle-orm/neon-http";
 
 export async function createResult(
   db: NeonHttpDatabase,
-  { userId, word, duration, date }: NewResult,
+  { userId, word, duration }: NewResult,
 ) {
   const inserted = await db
     .insert(resultsTable)
-    .values({ userId, word, duration, date })
+    .values({ userId, word, duration })
     .returning();
 
   return inserted[0];
-}
-
-export async function addErrorDate(
-  db: NeonHttpDatabase,
-  {
-    userId,
-    word,
-    errorDate,
-  }: {
-    userId: string;
-    word: string;
-    errorDate: string;
-  },
-) {
-  const updated = await db
-    .update(resultsTable)
-    .set({ errorDate })
-    .where(
-      and(
-        eq(resultsTable.word, word),
-        eq(resultsTable.userId, userId),
-        isNull(resultsTable.errorDate),
-      ),
-    )
-    .returning();
-
-  return updated;
 }
 
 export async function getAnalyticsPerWord(
@@ -50,24 +23,38 @@ export async function getAnalyticsPerWord(
     currentWords,
   }: { userId: string; repetitions: number; currentWords: string[] },
 ) {
+  const sq = db
+    .select()
+    .from(resultsTable)
+    .orderBy(desc(resultsTable.date))
+    .where(
+      and(
+        eq(resultsTable.userId, userId),
+        inArray(resultsTable.word, currentWords),
+      ),
+    )
+    .as("sq");
+
   const results = await db
     .select({
-      count: sql<number>`count(${resultsTable.id})`,
-      duration: sql<number>`avg(${resultsTable.duration})`,
-      word: resultsTable.word,
+      word: sq.word,
+      count: sql<number>`count(${sq.id})`,
+      lastDurations: sql<
+        number[]
+      >`(array_agg(${sq.duration}))[1:${repetitions}]`,
     })
-    .from(resultsTable)
-    .where(and(eq(resultsTable.userId, userId), isNull(resultsTable.errorDate)))
-    .groupBy(resultsTable.word);
+    .from(sq)
+    .groupBy(sq.word);
 
   const filteredResults = results.filter((r) => currentWords.includes(r.word));
 
   const analytics = filteredResults
     .filter((r) => r.count >= repetitions)
     .map((r) => {
+      const maxDuration = Math.max(...r.lastDurations);
       return {
         word: r.word,
-        speed: (((r.word.length + 1) / (r.duration / 1000)) * 60) / 5,
+        speed: (((r.word.length + 1) / (maxDuration / 1000)) * 60) / 5,
       };
     })
     .sort((a, b) => b.speed - a.speed);
